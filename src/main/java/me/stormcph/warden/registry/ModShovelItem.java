bbbf3b971e5e7f058e33265cf4fec762882e1c29
wardenPP <109866583+wardenPP@users.noreply.github.com>
2022-08-07 10:26:25 +0200

    Add files via upload

---
+++ src/main/java/me/stormcph/warden/registry/ModShovelItem.java
@@ -0,0 +1,10 @@
+package me.stormcph.warden.registry;
+
+import net.minecraft.item.ShovelItem;
+import net.minecraft.item.ToolMaterial;
+
+public class ModShovelItem extends ShovelItem {
+    public ModShovelItem(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
